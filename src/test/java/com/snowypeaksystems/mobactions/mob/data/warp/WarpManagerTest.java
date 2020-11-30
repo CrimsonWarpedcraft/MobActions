07d07bccd3fffac4438bcc8aa42ecd397ed4ac16
Levi Muniz <levi.muniz17@gmail.com>
2020-11-29 18:01:09 -0700

    Add WarpManager test skeleton

---
+++ src/test/java/com/snowypeaksystems/mobactions/mob/data/warp/WarpManagerTest.java
@@ -0,0 +1,30 @@
+package com.snowypeaksystems.mobactions.mob.data.warp;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for the WarpManager class.
+ * @author Copyright (c) Levi Muniz. All Rights Reserved.
+ */
+class WarpManagerTest {
+
+  @Test
+  void makeWarp() {
+  }
+
+  @Test
+  void getWarp() {
+  }
+
+  @Test
+  void unregister() {
+  }
+
+  @Test
+  void exists() {
+  }
+
+  @Test
+  void getLoadedWarpNames() {
+  }
+}
