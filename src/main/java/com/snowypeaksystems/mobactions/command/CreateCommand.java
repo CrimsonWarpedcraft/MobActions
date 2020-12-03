ef9bbafcae5f7bdf60ede0e18ff80a7f184543f4
Levi Muniz <levi.muniz17@gmail.com>
2020-12-02 22:32:42 -0700

    Implement CreateCommand

--- src/main/java/com/snowypeaksystems/mobactions/command/CreateCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/CreateCommand.java
@@ -1,11 +1,29 @@
 package com.snowypeaksystems.mobactions.command;
 
+import static com.snowypeaksystems.mobactions.util.Messages.gm;
+
+import com.snowypeaksystems.mobactions.data.MobData;
+import com.snowypeaksystems.mobactions.player.IMobActionsPlayer;
+import com.snowypeaksystems.mobactions.player.IStatus;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
 
 public class CreateCommand implements ICreateCommand {
+  private final IMobActionsPlayer player;
+  private final MobData data;
+
+  public CreateCommand(IMobActionsPlayer player, MobData data) {
+    this.player = player;
+    this.data = data;
+  }
 
   @Override
   public void run() throws PlayerException {
+    if (!player.canCreate()) {
+      throw new PermissionException(gm("permission-exception"));
+    }
 
+    player.getStatus().setMode(IStatus.Mode.CREATING);
+    player.getStatus().setMobData(data);
   }
 }
