ab885475a5b914af00ea404fd4d62d3ee1cdadcb
Levi Muniz <levi.muniz17@gmail.com>
2020-12-12 17:05:15 -0700

    Implement RemoveCommand

--- src/main/java/com/snowypeaksystems/mobactions/command/RemoveCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/RemoveCommand.java
@@ -1,11 +1,23 @@
 package com.snowypeaksystems.mobactions.command;
 
+import com.snowypeaksystems.mobactions.player.IStatus;
+import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
 
 public class RemoveCommand implements IRemoveCommand {
+  private final MobActionsUser player;
+
+  public RemoveCommand(MobActionsUser player) {
+    this.player = player;
+  }
 
   @Override
   public void run() throws PlayerException {
+    if (!player.canRemove()) {
+      throw new PermissionException();
+    }
 
+    player.getStatus().setMode(IStatus.Mode.DESTROYING);
   }
 }
