bd7c3ae9113c6b05600f1619d894db15b007d3de
Levi Muniz <levi.muniz17@gmail.com>
2020-12-14 23:22:17 -0700

    Removed future from CommandAction constructor
    There's not much of a need for it here since there's nothing we do when a command completes

--- src/main/java/com/snowypeaksystems/mobactions/actions/CommandAction.java
+++ src/main/java/com/snowypeaksystems/mobactions/actions/CommandAction.java
@@ -4,24 +4,19 @@ import com.snowypeaksystems.mobactions.data.ICommandData;
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
 import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
-import java.util.concurrent.CompletableFuture;
 
 public class CommandAction implements ICommandAction {
   private final ICommandData command;
   private final MobActionsUser player;
-  private final CompletableFuture<Boolean> future;
 
   /**
    * Creates a CommandAction for a command to be ran by a player.
    * @param player the player running the command
    * @param command the command that the player will run
-   * @param future the code to run if the execution is successful
    */
-  public CommandAction(MobActionsUser player, ICommandData command,
-                       CompletableFuture<Boolean> future) {
+  public CommandAction(MobActionsUser player, ICommandData command) {
     this.player = player;
     this.command = command;
-    this.future = future;
   }
 
   @Override
@@ -30,6 +25,6 @@ public class CommandAction implements ICommandAction {
       throw new PermissionException();
     }
 
-    future.complete(player.performCommand(command.replace(player.getDisplayName())));
+    command.replace(player.getDisplayName());
   }
 }
