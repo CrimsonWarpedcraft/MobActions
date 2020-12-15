2902b005e6e2ef028a0457bbb084ba8ffb4c45d2
Levi Muniz <levi.muniz17@gmail.com>
2020-12-14 23:57:02 -0700

    Implement EventListener#onMobInteract()

--- src/main/java/com/snowypeaksystems/mobactions/listener/EventListener.java
+++ src/main/java/com/snowypeaksystems/mobactions/listener/EventListener.java
@@ -1,15 +1,28 @@
 package com.snowypeaksystems.mobactions.listener;
 
+import static com.snowypeaksystems.mobactions.util.Messages.gm;
+
 import com.snowypeaksystems.mobactions.AMobActions;
 import com.snowypeaksystems.mobactions.IInteractiveMob;
+import com.snowypeaksystems.mobactions.actions.CommandAction;
+import com.snowypeaksystems.mobactions.actions.CreateAction;
+import com.snowypeaksystems.mobactions.actions.RemoveAction;
+import com.snowypeaksystems.mobactions.actions.WarpAction;
+import com.snowypeaksystems.mobactions.data.ICommandData;
+import com.snowypeaksystems.mobactions.data.IWarpData;
+import com.snowypeaksystems.mobactions.data.MobData;
+import com.snowypeaksystems.mobactions.player.IStatus;
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PlayerException;
 import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 /**
  * Handles all events relevant to the plugin, excluding command handlers.
@@ -26,15 +39,59 @@ public class EventListener implements IEventListener {
 
   @Override
   public void onMobInteract(PlayerInteractEntityEvent event) {
-    /*\\
     if (event.getHand().equals(EquipmentSlot.OFF_HAND)
         || !(event.getRightClicked() instanceof LivingEntity)
         || event.getRightClicked() instanceof Player) {
       return;
     }
-    */
 
-    //TODO: The rest
+    MobActionsUser player = ma.getPlayer(event.getPlayer());
+    IInteractiveMob mob = ma.getInteractiveMob((LivingEntity) event.getRightClicked());
+    if (player.getStatus().getMode() == IStatus.Mode.CREATING) {
+      event.setCancelled(true);
+      MobData data = player.getStatus().getMobData();
+
+      try {
+        (new CreateAction(player, mob, data)).run();
+      } catch (PlayerException e) {
+        player.sendMessage(e.getMessage());
+      }
+
+    } else if (player.getStatus().getMode() == IStatus.Mode.DESTROYING) {
+      event.setCancelled(true);
+
+      try {
+        (new RemoveAction(player, mob)).run();
+      } catch (PlayerException e) {
+        player.sendMessage(e.getMessage());
+      }
+
+    } else if (mob.getData() instanceof ICommandData) {
+      event.setCancelled(true);
+
+      try {
+        (new CommandAction(player, (ICommandData) mob.getData())).run();
+      } catch (PlayerException e) {
+        player.sendMessage(e.getMessage());
+      }
+
+    } else if (mob.getData() instanceof IWarpData) {
+      event.setCancelled(true);
+
+      CompletableFuture<Boolean> future = new CompletableFuture<>();
+      future.thenAccept(success -> {
+        if (success) {
+          player.sendMessage(gm("warp-success", mob.getData().getAlias()));
+        }
+      });
+
+
+      try {
+        (new WarpAction(player, (IWarpData) mob.getData(), ma.getWarpManager(), future)).run();
+      } catch (PlayerException e) {
+        player.sendMessage(e.getMessage());
+      }
+    }
   }
 
   @Override
