ba3de56bf69c5bbea87e4f3a5c9dcfc7bb167e51
Mason <Bassettmason@gmail.com>
2020-12-17 15:55:55 -0800

    Implemented DelWarpCommand

--- src/main/java/com/snowypeaksystems/mobactions/command/DelWarpCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/DelWarpCommand.java
@@ -1,28 +1,38 @@
 package com.snowypeaksystems.mobactions.command;
 
+import com.snowypeaksystems.mobactions.player.IStatus;
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
+import com.snowypeaksystems.mobactions.player.WarpNotFoundException;
 import com.snowypeaksystems.mobactions.warp.IWarpManager;
 
 public class DelWarpCommand implements IDelWarpCommand {
-  // private String name;
-  // private IWarpManager warpManager;
-  // private MobActionsUser player;
+  private String name;
+  private IWarpManager warpManager;
+  private MobActionsUser player;
 
   /** Creates a DelWarpCommand object. Can be performed by the console. */
   public DelWarpCommand(MobActionsUser player, String name, IWarpManager warpManager) {
-    // this.name = name;
-    // this.player = player;
-    // this.warpManager = warpManager;
+    this.name = name;
+    this.player = player;
+    this.warpManager = warpManager;
   }
 
   @Override
   public void run() throws PlayerException {
-    // TODO Mason
-    /*
-    1. Check player's permission, throw error if insufficient
-    2. Check if (lowercase) warp exists, throw error if not
-    3. Delete (lowercase) IWarp using the warp manager
-     */
+
+    if (!player.canCreate()) {
+      throw new PermissionException();
+    }
+
+    String nameToLower = name.toLowerCase();
+
+    if (!warpManager.exists(nameToLower)) {
+      throw new WarpNotFoundException(nameToLower);
+    }
+
+    player.getStatus().setMode(IStatus.Mode.DESTROYING);
+    warpManager.unregister(nameToLower);
   }
 }
