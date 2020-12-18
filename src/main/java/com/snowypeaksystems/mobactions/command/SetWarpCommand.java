bf0ff67f73ff4902a2ebf007f60681869ebd8372
Mason Bassett <Bassettmason@gmail.com>
2020-12-17 17:24:05 -0800

    Implemented setWarpCommand (#28)

--- src/main/java/com/snowypeaksystems/mobactions/command/SetWarpCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/SetWarpCommand.java
@@ -1,28 +1,34 @@
 package com.snowypeaksystems.mobactions.command;
 
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
+import com.snowypeaksystems.mobactions.player.WarpExistsException;
 import com.snowypeaksystems.mobactions.warp.IWarpManager;
 
 public class SetWarpCommand implements ISetWarpCommand {
-  // private String name;
-  // private IWarpManager warpManager;
-  // private MobActionsUser player;
+  private String name;
+  private IWarpManager warpManager;
+  private MobActionsUser player;
 
   /** Creates a SetWarpCommand object. */
   public SetWarpCommand(MobActionsUser player, String name, IWarpManager warpManager) {
-    // this.name = name;
-    // this.player = player;
-    // this.warpManager = warpManager;
+    this.name = name;
+    this.player = player;
+    this.warpManager = warpManager;
   }
 
   @Override
   public void run() throws PlayerException {
-    // TODO Mason
-    /*
-    1. Check player's permission, throw error if insufficient
-    2. Check if (lowercase) warp exists, throw error if so
-    3. Make an IWarp (lowercase) using the warp manager
-     */
+
+    if (!player.canSetWarp()) {
+      throw new PermissionException();
+    }
+    if (warpManager.exists(name.toLowerCase())) {
+      throw new WarpExistsException();
+    }
+
+    warpManager.makeWarp(name, player.getLocation());
+
   }
 }
