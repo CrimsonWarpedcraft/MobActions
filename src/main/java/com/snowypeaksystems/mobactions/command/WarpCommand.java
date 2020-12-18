d0de26b7466f90f6cb43c81ca875ff66c2e4ce65
Mason Bassett <Bassettmason@gmail.com>
2020-12-17 17:01:18 -0800

    Implemented WarpCommand (#31)

--- src/main/java/com/snowypeaksystems/mobactions/command/WarpCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/WarpCommand.java
@@ -1,34 +1,43 @@
 package com.snowypeaksystems.mobactions.command;
 
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
+import com.snowypeaksystems.mobactions.player.WarpNotFoundException;
 import com.snowypeaksystems.mobactions.warp.IWarpManager;
+import io.papermc.lib.PaperLib;
 import java.util.concurrent.CompletableFuture;
+import org.bukkit.Location;
 
 public class WarpCommand implements IWarpCommand {
-  // private final String warp;
-  // private final IWarpManager warpManager;
-  // private final MobActionsUser player;
-  // private final CompletableFuture<Boolean> future;
+  private final String warp;
+  private final IWarpManager warpManager;
+  private final MobActionsUser player; // Why does the MobActionsUser interface not start with I
+  private final CompletableFuture<Boolean> future;
 
   /** Creates a warp command. */
   public WarpCommand(MobActionsUser player, String warp, IWarpManager warpManager,
                      CompletableFuture<Boolean> future) {
-    // this.warp = warp;
-    // this.player = player;
-    // this.future = future;
-    // this.warpManager = warpManager;
+    this.warp = warp;
+    this.player = player;
+    this.future = future;
+    this.warpManager = warpManager;
   }
 
   @Override
   public void run() throws PlayerException {
-    //TODO Mason
-    /*
-    1. Check permissions, throw error if insufficient
-    2. Check if (lowercase) warp exists, throw error if not
-    3. Use code below to teleport the player to destination
-    PaperLib.getChunkAtAsync(warp.getDestination()).thenAccept(
-        chunk -> future.complete(player.getPlayer().teleport(warp.getDestination())));
-     */
+    if (!player.canUseWarpCommand()) {
+      throw new PermissionException();
+    }
+
+    String warpToLower = warp.toLowerCase();
+
+    if (!warpManager.exists(warpToLower)) {
+      throw new WarpNotFoundException(warpToLower);
+    }
+
+    Location location = warpManager.getWarp(warpToLower).getDestination();
+    PaperLib.getChunkAtAsync(location).thenAccept(
+        chunk -> future.complete(player.teleport(location)));
   }
 }
