642eb785587a23543bf4c6d1d929d2cd32e975b6
Levi Muniz <levi.muniz17@gmail.com>
2020-12-19 20:44:08 -0700

    Refactored ListWarpsCommand constructor

--- src/main/java/com/snowypeaksystems/mobactions/command/ListWarpsCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/ListWarpsCommand.java
@@ -2,19 +2,19 @@ package com.snowypeaksystems.mobactions.command;
 
 import static com.snowypeaksystems.mobactions.util.Messages.gm;
 
-import com.snowypeaksystems.mobactions.AMobActions;
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
 import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
+import com.snowypeaksystems.mobactions.warp.IWarpManager;
 import java.util.Set;
 
 public class ListWarpsCommand implements IListWarpsCommand {
-  private final AMobActions ma;
+  private final IWarpManager wm;
   private final MobActionsUser player;
 
-  public ListWarpsCommand(MobActionsUser player, AMobActions ma) {
+  public ListWarpsCommand(MobActionsUser player, IWarpManager wm) {
     this.player = player;
-    this.ma = ma;
+    this.wm = wm;
   }
 
   @Override
@@ -23,7 +23,7 @@ public class ListWarpsCommand implements IListWarpsCommand {
       throw new PermissionException();
     }
 
-    Set<String> warps = ma.getWarpManager().getLoadedWarpNames();
+    Set<String> warps = wm.getLoadedWarpNames();
     if (warps.size() > 0) {
       String warpsStr = String.join(", ", warps);
       player.sendMessage(gm("list-message"), warpsStr);
