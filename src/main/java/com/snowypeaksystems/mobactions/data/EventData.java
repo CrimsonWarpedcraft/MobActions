44ee93c96db7b27bdb0f9877321856e9b6cc3a0d
Levi Muniz <levi.muniz17@gmail.com>
2021-01-03 21:39:48 -0700

    Changes to EventData for new Event implementation strategy

--- src/main/java/com/snowypeaksystems/mobactions/data/EventData.java
+++ src/main/java/com/snowypeaksystems/mobactions/data/EventData.java
@@ -2,8 +2,6 @@ package com.snowypeaksystems.mobactions.data;
 
 import static com.snowypeaksystems.mobactions.util.Messages.gm;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -12,44 +10,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventData implements IEventData {
   private final String name;
-  private final MobData data;
 
   /** Create a new EventData object from a name, maxPlayers, and data. */
-  public EventData(String name, MobData data) {
+  public EventData(String name) {
     this.name = name;
-    this.data = data;
   }
 
   /** Create EventData from the supplied LivingEntity and plugin. */
   public EventData(LivingEntity entity, JavaPlugin plugin) throws IncompleteDataException {
     PersistentDataContainer container = entity.getPersistentDataContainer();
-    NamespacedKey aliasKey = new NamespacedKey(plugin, EVENT_ALIAS_KEY);
     NamespacedKey eventKey = new NamespacedKey(plugin, EVENT_KEY);
 
-    if (!container.has(aliasKey, PersistentDataType.STRING)
-        || !container.has(eventKey, PersistentDataType.STRING)) {
+    if (!container.has(eventKey, PersistentDataType.STRING)) {
       throw new IncompleteDataException();
     }
 
-    this.name = container.get(aliasKey, PersistentDataType.STRING);
-
-    try {
-      Constructor<? extends MobData> c = MobData.DATA_KEY_MAP.get(
-          container.get(eventKey, PersistentDataType.STRING))
-          .getConstructor(LivingEntity.class, JavaPlugin.class);
-      data = c.newInstance(entity, plugin);
-
-      if (data instanceof IEventData) {
-        throw new IllegalArgumentException("Cannot use stored EventData as an event's data");
-      }
-    } catch (InvocationTargetException | IllegalAccessException | InstantiationException
-        | NoSuchMethodException e) {
-      if (e.getCause() instanceof IncompleteDataException) {
-        throw (IncompleteDataException) e.getCause();
-      }
-
-      throw new IncompleteDataException();
-    }
+    this.name = container.get(eventKey, PersistentDataType.STRING);
   }
 
   @Override
@@ -58,36 +34,17 @@ public class EventData implements IEventData {
   }
 
   @Override
-  public MobData getEventData() {
-    return data;
-  }
-
-  @Override
   public void store(LivingEntity entity, JavaPlugin plugin) {
-    if (data != null) {
-      PersistentDataContainer container = entity.getPersistentDataContainer();
-      NamespacedKey aliasKey = new NamespacedKey(plugin, EVENT_ALIAS_KEY);
-      NamespacedKey eventKey = new NamespacedKey(plugin, EVENT_KEY);
-
-      container.set(aliasKey, PersistentDataType.STRING, name);
-      container.set(eventKey, PersistentDataType.STRING, data.getKeyString());
-
-      data.store(entity, plugin);
-    }
+    PersistentDataContainer container = entity.getPersistentDataContainer();
+    NamespacedKey eventKey = new NamespacedKey(plugin, EVENT_KEY);
+    container.set(eventKey, PersistentDataType.STRING, name);
   }
 
   @Override
   public void purge(LivingEntity entity, JavaPlugin plugin) {
     PersistentDataContainer container = entity.getPersistentDataContainer();
-    NamespacedKey aliasKey = new NamespacedKey(plugin, EVENT_ALIAS_KEY);
     NamespacedKey eventKey = new NamespacedKey(plugin, EVENT_KEY);
-
-    container.remove(aliasKey);
     container.remove(eventKey);
-
-    if (data != null) {
-      data.purge(entity, plugin);
-    }
   }
 
   @Override
--- src/main/java/com/snowypeaksystems/mobactions/data/IEventData.java
+++ src/main/java/com/snowypeaksystems/mobactions/data/IEventData.java
@@ -1,8 +1,5 @@
 package com.snowypeaksystems.mobactions.data;
 
-public interface IEventData extends MobData {
+public interface IEventData extends MobData, AliasedData {
   String EVENT_KEY = "event";
-  String EVENT_ALIAS_KEY = "event-alias";
-
-  MobData getEventData();
 }
