9fd33604e5bdc680b4fe35e3bcae5e990f6f838b
Levi Muniz <levi.muniz17@gmail.com>
2021-01-17 23:34:58 -0700

    No longer require any permission to list warps

--- src/main/java/com/snowypeaksystems/mobactions/command/ListWarpsCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/ListWarpsCommand.java
@@ -3,7 +3,6 @@ package com.snowypeaksystems.mobactions.command;
 import static com.snowypeaksystems.mobactions.util.Messages.gm;
 
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
-import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
 import com.snowypeaksystems.mobactions.util.DebugLogger;
 import com.snowypeaksystems.mobactions.warp.IWarpManager;
@@ -19,18 +18,15 @@ public class ListWarpsCommand implements IListWarpsCommand {
   @Override
   public void run(MobActionsUser player) throws PlayerException {
     DebugLogger.getLogger().log("Listing warps");
-    if (!player.canUseWarpsCommand()) {
-      DebugLogger.getLogger().log("Permission error");
-      throw new PermissionException();
-    }
-
     Set<String> warps = wm.getLoadedWarpNames();
+
     if (warps.size() > 0) {
       String warpsStr = String.join(", ", warps);
       player.sendMessage(gm("list-message"), warpsStr);
     } else {
       player.sendMessage(gm("list-empty-message"));
     }
+    
     DebugLogger.getLogger().log("Warps listed");
   }
 }
