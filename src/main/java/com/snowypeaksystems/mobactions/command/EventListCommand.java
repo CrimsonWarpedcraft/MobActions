e011140e60e1a58b179dae4c99dda823c3a961c6
Mason <Bassettmason@gmail.com>
2021-01-20 22:12:26 -0800

    Fix Permission Issue

--- src/main/java/com/snowypeaksystems/mobactions/command/EventListCommand.java
+++ src/main/java/com/snowypeaksystems/mobactions/command/EventListCommand.java
@@ -4,6 +4,7 @@ import static com.snowypeaksystems.mobactions.util.Messages.gm;
 
 import com.snowypeaksystems.mobactions.mobevent.IMobEventManager;
 import com.snowypeaksystems.mobactions.player.MobActionsUser;
+import com.snowypeaksystems.mobactions.player.PermissionException;
 import com.snowypeaksystems.mobactions.player.PlayerException;
 import com.snowypeaksystems.mobactions.util.DebugLogger;
 import java.util.Set;
@@ -18,6 +19,11 @@ public class EventListCommand implements IEventListCommand {
   @Override
   public void run(MobActionsUser player) throws PlayerException {
     DebugLogger.getLogger().log("Listing events");
+    if (!player.canGetEventInfo()) {
+      DebugLogger.getLogger().log("Permission error");
+      throw new PermissionException();
+    }
+
     Set<String> events = em.getLoadedEventNames();
 
     if (events.size() > 0) {
